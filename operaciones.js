7fd783b2d583301a3d844b44cb5a729355c31972
Jenny <114629733+jennygong98@users.noreply.github.com>
2023-01-25 21:09:51 -0600

    Create operaciones.js
    Desarrollador 1

--- operaciones.js
+++ operaciones.js
@@ -8,10 +8,36 @@ function resta(a, b) {
     return a - b;
 }
 
+// Desarrollador 1 Jenny Gong
+function multiplicacion (a, b){ 
+    return a * b; 
+} 
+
+// Desarrollador 4
+
+
+//Desarrollador 1 Jenny Gong
+function potencia(base, exponente){
+    return Math.pow(base,exponente);
+}
+
+
 // Desarrollador 2 
 function raizCuadrada(n) { 
     return Math.sqrt(n); 
 } 
+
+// Desarrollador 4
+
+// Desarrollador 1 Jenny Gong
+function promedio(numeros){
+    let suma = 0;
+    for (let i=0;i< numeros.length; i++){
+        suma +=numeros [i];}
+    return suma / numeros.length;
+}
+
+// Desarrollador 4
 
 // Desarrollador 3 Brandon Loria
 function maximo(numeros) {
@@ -50,14 +76,45 @@ console.log(potencia(2, 3));
  
 console.log(raizCuadrada(9));
 
+// Desarrollador 1 Jenny Gong
+console.log(porcentaje(100,2));
+console.log(promedio([2, 3, 5, 7, 11]));
+
 // Desarrollador 3 Brandon Loria
 
 console.log(factorial(5));
 
 console.log(maximo([2, 3, 5, 7, 11]));
 
+// Desarrollador 4
 
+// Desarrollador 1 
+let frutas = ["manzanas", "plátano","pera", "mango"];
 
+// Desarrollador 2 
+function imprimirFrutas() { 
+    console.log(frutas); 
+} 
 
+// Desarrollador 3 Brandon Loria
+function agregarFruta(fruta) {
+    frutas.push(fruta);
+}
 
+// Desarrollador 2 
+function eliminarFruta(fruta) { 
+    let index = frutas.indexOf(fruta); 
+    if (index > -1) { 
+        frutas.splice(index, 1); 
+    } 
+} 
+
+// Desarrollador 4
+
+// Desarrollador 4
+
+// Desarrollador 3
+
+// Desarrollador 1
+imprimirFrutas();
 
