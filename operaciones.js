1f711ca4d95d8583ba3d40735c78b46fb553ecb0
BrandonMld <123426682+BrandonMld@users.noreply.github.com>
2023-01-25 20:51:09 -0600

    Update operaciones.js

--- operaciones.js
+++ operaciones.js
@@ -3,10 +3,27 @@ function suma(a, b) {
     return a + b; 
 } 
 
+// Desarrollador 3 Brandon Loria
+function resta(a, b) {
+    return a - b;
+}
+
 // Desarrollador 2 
 function raizCuadrada(n) { 
     return Math.sqrt(n); 
 } 
+
+// Desarrollador 3 Brandon Loria
+function maximo(numeros) {
+    let max = numeros[0];
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeros[i] > max) {
+            max = numeros[i];
+        }
+    }
+    return max;
+}
+
 / Desarrollador 2 
 function minimo(numeros) { 
     let min = numeros[0]; 
@@ -17,18 +34,40 @@ function minimo(numeros) {
     } 
     return min; 
 } 
+// Desarrollador 3 Brandon Loria
+
+console.log(suma(2, 3)); 
+
+console.log(resta(5, 3)); 
+
+console.log(multiplicacion(2, 3));
+
 // Desarrollador 2 
  
 console.log(division(6, 3)); 
  
 console.log(potencia(2, 3)); 
  
-console.log(raizCuadrada(9)); 
+console.log(raizCuadrada(9));
+
+// Desarrollador 3 Brandon Loria
+
+console.log(factorial(5));
+
+console.log(maximo([2, 3, 5, 7, 11]));
+
+
 
 // Desarrollador 2 
 function imprimirFrutas() { 
     console.log(frutas); 
 } 
+
+// Desarrollador 3 Brandon Loria
+function agregarFruta(fruta) {
+    frutas.push(fruta);
+}
+
 / Desarrollador 2 
 function eliminarFruta(fruta) { 
     let index = frutas.indexOf(fruta); 
@@ -36,4 +75,8 @@ function eliminarFruta(fruta) {
         frutas.splice(index, 1); 
     } 
 } 
+// Desarrollador 3 Brandon Loria
+imprimirFrutas(); 
+
+eliminarFruta("plátano");
 
