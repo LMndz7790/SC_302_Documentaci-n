dcdfb25ab70618a30b7c67209b2e3b7de51ffc74
Fernando1603 <123424958+Fernando1603@users.noreply.github.com>
2023-01-28 09:27:08 -0600

    Update frutas.js

--- frutas.js
+++ frutas.js
@@ -5,3 +5,37 @@ function agregarFruta(fruta) {
 
 // Desarrollador 3 Brandon Loria
 imprimirFrutas(); 
+// Desarrollador 1 
+
+let frutas = ["manzana", "plátano", "pera", "mango"]; 
+
+ 
+
+// Desarrollador 2 
+
+
+// Desarrollador 3 
+
+
+// Desarrollador 2 
+
+
+// Desarrollador 4 Fernando Chacon
+
+imprimirFrutas();  
+
+ 
+
+// Desarrollador 4 Fernando Chacon
+
+agregarFruta("kiwi"); 
+
+ 
+
+// Desarrollador 3 
+
+
+ 
+
+// Desarrollador 1 
+
