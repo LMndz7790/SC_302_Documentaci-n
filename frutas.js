c60246d19ab39cc76c472714346688d5337397bb
LMndz7790 <83137958+LMndz7790@users.noreply.github.com>
2023-01-28 09:46:48 -0600

    Update frutas.js

--- frutas.js
+++ frutas.js
@@ -2,16 +2,25 @@
 
 let frutas = ["manzana", "plátano", "pera", "mango"]; 
 
-// Desarrollador 2
+// Desarrollador 2 Luis Méndez B.
+
+function imprimirFrutas() {
+    console.log(frutas);
+}
+
 
 // Desarrollador 3 Brandon Loria
 function agregarFruta(fruta) {
     frutas.push(fruta);
 }
 
-// Desarrollador 2 
-
-
+// Desarrollador 2 Luis Méndez B.
+function eliminarFruta(fruta) {
+    let index = frutas.indexOf(fruta);
+    if (index > -1) {
+        frutas.splice(index, 1);
+    }
+}
 
 // Desarrollador 4 Fernando Chacon
 imprimirFrutas();  
